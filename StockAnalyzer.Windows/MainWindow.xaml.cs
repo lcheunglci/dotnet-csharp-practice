c3948329b2d1b4b5009fb425684fd9669b3a1ed8
Lawrence LCI <lcheunglci@gmail.com>
2023-09-24 23:28:11 -0700

    Update the analyzer to read synchronously from a file

--- StockAnalyzer.Windows/MainWindow.xaml.cs
+++ StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using StockAnalyzer.Core.Domain;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
@@ -25,12 +27,23 @@ public partial class MainWindow : Window
 
 
 
-    private async void Search_Click(object sender, RoutedEventArgs e)
+    private void Search_Click(object sender, RoutedEventArgs e)
     {
         try
         {
             BeforeLoadingStockData();
-            await GetStocks();
+            var lines = File.ReadAllLines("StockPrices_Small.csv");
+
+            var data = new List<StockPrice>();
+
+            foreach (var line in lines.Skip(1))
+            {
+                var price = StockPrice.FromCSV(line);
+
+                data.Add(price);
+            }
+
+            Stocks.ItemsSource = data.Where(sp => sp.Identifier == StockIdentifier.Text);
         }
         catch (Exception ex)
         {
