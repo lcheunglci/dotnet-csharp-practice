921375161b4353057e648e4e1d7ffa48debba2e8
Lawrence LCI <lcheunglci@gmail.com>
2023-10-05 23:35:27 -0700

    Create a sample project to show detached child tasks

---
+++ StockAnalyzer.AttachedDetatched/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Starting");
+
+Task.Factory.StartNew(() =>
+{
+    Task.Factory.StartNew(() =>
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine("Completed 1");
+    });
+    Task.Factory.StartNew(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("Completed 2");
+    });
+    Task.Factory.StartNew(() =>
+    {
+        Thread.Sleep(3000);
+        Console.WriteLine("Completed 3");
+    });
+});
+
+Console.WriteLine("Completed");
+Console.ReadLine();
