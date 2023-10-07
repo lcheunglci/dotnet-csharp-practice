6cd92fe40ff990c2726b484aa98906ff2fef814b
Lawrence LCI <lcheunglci@gmail.com>
2023-10-06 23:03:07 -0700

    Use attached to parent option to keep the execution order

--- StockAnalyzer.AttachedDetatched/Program.cs
+++ StockAnalyzer.AttachedDetatched/Program.cs
@@ -7,17 +7,17 @@ Task.Factory.StartNew(() =>
     {
         Thread.Sleep(1000);
         Console.WriteLine("Completed 1");
-    });
+    }, TaskCreationOptions.AttachedToParent);
     Task.Factory.StartNew(() =>
     {
         Thread.Sleep(2000);
         Console.WriteLine("Completed 2");
-    });
+    }, TaskCreationOptions.AttachedToParent);
     Task.Factory.StartNew(() =>
     {
         Thread.Sleep(3000);
         Console.WriteLine("Completed 3");
-    });
+    }, TaskCreationOptions.AttachedToParent);
 });
 
 Console.WriteLine("Completed");
