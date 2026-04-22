fc17f6f88894b7c1df0460ebc2e1ec0e1c328049
Lawrence LCI <lcheunglci@gmail.com>
2026-04-21 23:13:59 -0700

    feat: prevent unsupported TFM

--- class-demo/calc-demo/Demo.Math/Calculator.cs
+++ class-demo/calc-demo/Demo.Math/Calculator.cs
@@ -1,24 +1,40 @@
 ﻿namespace Demo.Math
 {
-  public static class Calculator
-  {
-    public static int Add(int a, int b)
+    public static class Calculator
     {
-      return a + b;
-    }
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
 
-    public static string AsHex(int a)
-    {
+        public static string AsHex(int a)
+        {
 
-      var hex = a.ToString("X");
+            var hex = a.ToString("X");
 #if NET6_0_OR_GREATER
-      return $"{hex} from .NET 6 or greater";
+            return $"{hex} from .NET 6 or greater";
 #elif NETFRAMEWORK
       return $"{hex} from .NET Framework";
 #else
       return $"{hex} from .NET Standard 2.0";
 #endif
+        }
+
+        public static void WriteAsHex(int a)
+        {
+
+            var hex = a.ToString("X");
+#if NET6_0_OR_GREATER
+            Console.WriteLine($"{hex} from .NET 6 or greater");
+#elif NETFRAMEWORK
+            System.Console.WriteLine($"{hex} from .NET Framework");
+#elif NETSTANDARD2_0
+            System.Console.WriteLine($"{hex} from .NET Standard 2.0");
+#else
+#error No TFM-specific code has been implemented
+            // throw new System.PlatformNotSupportedException("This code should not be reached, as the preprocessor should prevent it.");
+#endif
+        }
     }
-  }
 
 }
