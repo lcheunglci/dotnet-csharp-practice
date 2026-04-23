52fd5c443083bd44cb6b72c78429b85468fcda07
Lawrence LCI <lcheunglci@gmail.com>
2026-04-22 23:24:40 -0700

    test: add calc test

---
+++ class-demo/calc-demo/Demo.Math.Test2/CalculatorTests.cs
@@ -0,0 +1,15 @@
+﻿using Demo.Math;
+
+namespace Demo.Math.Test2
+{
+    public class Tests
+    {
+        [Test]
+        public void Add2PositiveNumbers()
+        {
+            var result = Calculator.Add(40, 2);
+
+            Assert.That(result).Is.EqualTo(42);
+        }
+    }
+}
